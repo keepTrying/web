d42ef793a7f797fc89dc3b1ae958287dbcee2642
Yang Peidong <yangpeidong@smalltreemedia.com>
2016-04-20 16:42:55 +0800

    no message

--- li/js/main.js
+++ li/js/main.js
@@ -1,26 +1,10 @@
 ;$(function(){
-	$("#find").click(function(){
-		 $in = $("#input_in").val();
-		 $out = $("#input_out").val();
-		 $num = $("#select_num").val().substr(0,1);
-		alert($in+$out+$num);
-            window.location.href="yuding.html?in="+$in+"&out="+$out+"&num="+$num;
-	});
     
     //数字验证  
     $("#getcode_num").click(function(){  
         $(this).attr("src",'../hotel/code_num.php?' + Math.random());  
     });  
-    $("#chk_num").click(function(){  
-        var code_num = $("#code_num").val();  
-        $.post("../hotel/chk_code.php?act=num",{code:code_num},function(msg){  
-            if(msg==1){  
-                alert("验证码正确！");  
-            }else{  
-                alert("验证码错误！");  
-            }  
-        });  
-    });  
+
  
 })
 ;function strtodate(str){
