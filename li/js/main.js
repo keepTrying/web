592d122cbb4cf4977951bfe8e1f69ff7cefa4625
Yang Peidong <yangpeidong@smalltreemedia.com>
2016-04-20 14:34:36 +0800

    no message

--- li/js/main.js
+++ li/js/main.js
@@ -6,4 +6,18 @@
 		alert($in+$out+$num);
             window.location.href="yuding.html?in="+$in+"&out="+$out+"&num="+$num;
 	});
-});
+})
+;function strtodate(str){
+    str=str.replace(/-/g,"/");
+    return new Date(str);
+}
+;function istimeconfict(time1,time2,time3,time4){
+    time1=strtodate(time1);
+    time2=strtodate(time2);
+    time3=strtodate(time3);
+    time4=strtodate(time4);
+    if((time1<=time3&&time2>=time3)||(time1>time3&&time1<time4))
+        return true;
+    else
+        return false;
+}    
