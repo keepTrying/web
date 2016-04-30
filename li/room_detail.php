bf909608bd5f0a44ca888960f4d8f3fc4c3821ea
KeepTrying <382858146@qq.com>
2016-04-30 08:04:31 +0800

    no message

--- li/room_detail.php
+++ li/room_detail.php
@@ -429,11 +429,10 @@
             page: "0",
             num_page: "999"
         }, function(data, status) {
-            var obj = $.parseJSON(data);
+            var obj = eval(data);
             if (obj.status == '200') {
 
-                var i;
-                for (i in obj.data) {
+                for (var i in obj.data) {
                         alert(i.user_id);
                     $.post('http://42.96.148.66/hotel/user/query.php', {
                         user_nick: '',
